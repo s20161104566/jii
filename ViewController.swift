2886d6492e3aaed730b3fde9bcf3be244c8f2b67
s20161104566 <a20161104566@mac04.local>
2018-09-26 11:32:11 +0800

    mmm
    Mmm

---
+++ ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  小猪1
+//
+//  Created by 20161104566 on 2018/9/26.
+//  Copyright © 2018年 20161104566. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction func one(_ sender: Any) {
+    }
+    @IBAction func two(_ sender: Any) {
+    }
+    @IBAction func three(_ sender: Any) {
+    }
+    @IBAction func four(_ sender: Any) {
+    }
+    @IBAction func five(_ sender: Any) {
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
